0ca65971a8a1e9a918119c5dc3fb5189e5acb54d
Jamis Charles <jacharles@paypal.com>
2018-07-20 16:11:50 -0500

    final touches

---
+++ README.md
@@ -0,0 +1,22 @@
+# Go chat server 
+
+## Start instructions
+### To run the chat server:
+`$ go run *.go`
+
+### To connect a telnet client:
+`telnet localhost 3333`
+
+### To post messages via curl:
+`curl -d 'username=myFancyUserName&msg=How do you do fellow kids?' 'http://localhost:3000/post'`
+
+### To check messages history via curl:
+`curl 'http://localhost:3000/messages'`
+
+
+## Notes:
+There aren't any bugs I'm aware of. There is currently only 1 big chatroom. I'm using channels to broadcast messages between all the connected clients. After you connect via telnet you can type `/help` to se what commands are available.
+
+All chat messages are saved to `./messages.log`.
+
+Given more time, I would add tests, add proper error conditions, and ensure I'm closing the channels and connections properly. Following that, I'd start testing scale, and see how the server scales before optimizing.  
--- main.go
+++ main.go
@@ -14,93 +14,10 @@ import (
 	"time"
 )
 
-/*
-TODO:
-- send message to all the people... (channels?) - DONE
-	- Don't subscribe to your own messages...
-- store some global state
-	- for id, name etc
-- don't send messages that you sent...
-
-- allow override from a config file...
-- implement telnet protocol ( try manually first, then use package)
-- add proper err handling
-- make sure I'm following Go style
-
-- tests?
-
-*/
-
-// FIXME: move somewhere else...
 type ChatMessage struct {
 	msg string
-	id  int // FIXME: change this type?
+	id  int
 }
-
-// TODO: move all the broker stuff into a separate file...?
-type Broker struct {
-	stopCh    chan struct{}
-	publishCh chan interface{}
-	subCh     chan chan interface{}
-	unsubCh   chan chan interface{}
-}
-
-func NewBroker() *Broker {
-	return &Broker{
-		stopCh:    make(chan struct{}),
-		publishCh: make(chan interface{}, 1),
-		subCh:     make(chan chan interface{}, 1),
-		unsubCh:   make(chan chan interface{}, 1),
-	}
-}
-
-func (b *Broker) Start() {
-	subs := map[chan interface{}]struct{}{}
-	for {
-		select {
-		case <-b.stopCh:
-			return
-		case msgCh := <-b.subCh:
-			subs[msgCh] = struct{}{}
-		case msgCh := <-b.unsubCh:
-			delete(subs, msgCh)
-		case msg := <-b.publishCh:
-			for msgCh := range subs {
-				// msgCh is buffered, use non-blocking send to protect the broker:
-				select {
-				case msgCh <- msg:
-				default:
-				}
-			}
-		}
-	}
-}
-
-func (b *Broker) Stop() {
-	close(b.stopCh)
-}
-
-func (b *Broker) Subscribe() chan interface{} {
-	msgCh := make(chan interface{}, 5)
-	b.subCh <- msgCh
-	return msgCh
-}
-
-func (b *Broker) Unsubscribe(msgCh chan interface{}) {
-	b.unsubCh <- msgCh
-}
-
-func (b *Broker) Publish(msg interface{}) {
-	b.publishCh <- msg
-}
-
-// END broker file?
-
-// const (
-// 	CONN_HOST = "localhost"
-// 	CONN_PORT = "3333"
-// 	CONN_TYPE = "tcp"
-// )
 
 type User struct {
 	id       int
@@ -108,45 +25,44 @@ type User struct {
 	muteList map[int]string //users to mute by id
 }
 
+type Config struct {
+	Host     string `json:"host"`
+	Port     string `json:"port"`
+	ConnType string `json:"connType"`
+	LogPath  string `json:"logFilePath"`
+}
+
+// used for HTTP api
 type HttpAPI struct {
 	b   *Broker
 	cfg Config
 }
 
-// global var. FIXME: good or bad?
-// FIXME: just change this to array?
-// var state = map[int]string{0: "System"} // init empty map
-// var state = map[int]map[string]string{} // init empty map
+// Global vars
 var state = []User{}
 var userNameList = map[string]int{} // map userNames to user ID
-var msgHistory []string             // keep last 100 messages in mem
-
-// state = append(state, systemUser)
-
-// var state = map[int]map[string]string{0: {"name": "System", "mute":[]} } // init empty map
+var msgHistory []string             // keep last 30 messages in mem
 
 func main() {
 
-	addNewUser("system")
-
 	cfg := readConfigFromFile()
 
-	// create and start the chat message broker
-	// create one of these for each room?
+	// add system user for system notifications
+	addNewUser("system")
+
+	// create and start our primary chatroom
 	b := NewBroker()
 	go b.Start()
 
+	// setup http server for http API access
 	api := &HttpAPI{cfg: cfg, b: b}
-
-	// just use different port?
-	// anon function as goroutine
 	go func() {
 		http.HandleFunc("/messages", httpHistoryHandler)
 		http.HandleFunc("/post", api.httpPosthandler)
 		log.Fatal(http.ListenAndServe(":3000", nil))
 	}()
 
-	// Listen for incoming connections.
+	// Listen for incoming tcp connections ie: telnet
 	ln, err := net.Listen(cfg.ConnType, cfg.Host+":"+cfg.Port)
 
 	if err != nil {
@@ -156,13 +72,11 @@ func main() {
 
 	defer ln.Close()
 
-	// FIXME: move this into separate fn
-	// I think this runs until all the goroutines close
+	// handle new incoming connections
 	for {
 		conn, err := ln.Accept()
 		if err != nil {
 			fmt.Println("Error handling connection:", err.Error())
-			// handle error
 		}
 
 		id := addNewUser("")
@@ -172,21 +86,20 @@ func main() {
 
 }
 
+/**
+* HTTP handlers for http requests
+**/
 func httpHistoryHandler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "Here are the most recent messages: \n%s", getMsgHistory())
 }
 func (api *HttpAPI) httpPosthandler(w http.ResponseWriter, r *http.Request) {
 	var userId int
-	// body: pass username, msgbody
 	r.ParseForm()
 
-	// FIXME: check for params, and respond with error if some are missing
+	// TODO: check for params, and respond with error if some are missing
 	username := r.Form["username"][0]
 	msg := r.Form["msg"][0]
 
-	// FIXME: get the user, or create a new one with that name ...
-	// get user by name and if it doesn't exist, create it...
-	// make them use their email address
 	// TODO: add proper authentication
 	usernameExists := hasUserNameBeenTaken(username)
 	if usernameExists == true {
@@ -195,20 +108,15 @@ func (api *HttpAPI) httpPosthandler(w http.ResponseWriter, r *http.Request) {
 		userId = addNewUser(username)
 	}
 
-	// auto-increment this person...
 	sendMessage(msg, userId, api.b, api.cfg)
 
 	fmt.Fprintf(w, "Message successfully posted: \n%s", msgHistory[len(msgHistory)-1])
-
-	// fmt.Fprintf(w, "Here are the most recent messages: \n%s", getMsgHistory())
 }
 
-// Create and subscribe 3 clients:
-// FIXME: change this name...
-func clientFunc(id int, b *Broker, conn net.Conn) {
+// handles each message we (the client) get from the server
+func setupClientMsgHandler(id int, b *Broker, conn net.Conn) {
 	msgCh := b.Subscribe()
 	for {
-		// FIXME: can we change the return type to a ChatMessage instead of interface?
 		msgData := <-msgCh
 		msgDetails := msgData.(ChatMessage)
 
@@ -216,11 +124,9 @@ func clientFunc(id int, b *Broker, conn net.Conn) {
 		if id != msgDetails.id && isMessageAuthorMuted(id, msgDetails.id) == false {
 			conn.Write([]byte(msgDetails.msg))
 		}
-
 	}
 }
 
-// FIXME: make config global?
 func handleConnection(conn net.Conn, b *Broker, id int, cfg Config) {
 	remoteAddr := conn.RemoteAddr().String()
 	fmt.Printf("Client id: %d with username [%s] connected from %s\n", id, getUserName(id), remoteAddr)
@@ -230,20 +136,18 @@ func handleConnection(conn net.Conn, b *Broker, id int, cfg Config) {
 
 	printWelcomeMessage(conn, id)
 
-	// since we are turning this into a goroutine, it's kind of the same as non-blocking async stuff in node
-	go clientFunc(id, b, conn)
+	// subscribe to all messages
+	go setupClientMsgHandler(id, b, conn)
 
-	// subscribe to each message channel...
 	scanner := bufio.NewScanner(conn)
 
-	// scanner2 := bufio.NewScanner(queue)
-
+	// handles every message from the client
 	for {
 		ok := scanner.Scan()
 
 		msg := fmt.Sprintf(scanner.Text())
 
-		// if msg starts with / it's a command
+		// if msg starts with / process as command
 		if len(msg) > 0 && msg[0] == '/' {
 			handleCommand(msg, conn, id, b, cfg)
 		} else {
@@ -258,17 +162,99 @@ func handleConnection(conn net.Conn, b *Broker, id int, cfg Config) {
 	fmt.Println("Client at " + remoteAddr + " disconnected.")
 }
 
-// utils... FIXME: Move this to a utils (after I read the formatting doc)
+func handleCommand(message string, conn net.Conn, id int, b *Broker, cfg Config) {
+
+	if len(message) > 0 && message[0] == '/' {
+		switch {
+
+		case message == "/help":
+
+			msg := `> Commands you can run:
+/help - See commands available to you
+/history - See the last 30 messages
+/name - Change your username
+/mute [name] - mute another user
+/unmute [name] - unmute a user you have muted previously
+/quit - disconnect your client` + "\n"
+			// msg = formatMessage(msg, 0)
+			conn.Write([]byte(msg))
+
+		case message == "/history":
+			resp := strings.Join(msgHistory, "")
+			resp = "> Recent messages: \n" + resp
+			conn.Write([]byte(resp))
+
+		// starts with /name
+		case strings.HasPrefix(message, "/name "):
+			oldName := getUserName(id)
+			newName := strings.Replace(message, "/name ", "", -1)
+
+			nameTaken := hasUserNameBeenTaken(newName)
+
+			if nameTaken == true {
+				msg := "> I'm sorry. The userName [" + newName + "] has already been taken. Try another name\n"
+				conn.Write([]byte(msg))
+
+				return
+			}
+
+			changeUserName(id, newName)
+
+			// send as system message
+			msg := fmt.Sprintf("%s has changed their name to %s", oldName, newName)
+			sendMessage(msg, 0, b, cfg) // send system notification
+
+		case strings.HasPrefix(message, "/mute "):
+			userToMute := strings.Replace(message, "/mute ", "", -1)
+
+			muteUser(id, userToMute)
+
+			// success msg
+			msg := fmt.Sprintf("%s has muted %s", getUserName(id), userToMute)
+			sendMessage(msg, 0, b, cfg) // send system notification
+
+		case strings.HasPrefix(message, "/unmute "):
+			userToUnMute := strings.Replace(message, "/unmute ", "", -1)
+
+			unMuteUser(id, userToUnMute)
+
+			// success msg
+			msg := fmt.Sprintf("%s has UN-muted %s", getUserName(id), userToUnMute)
+			sendMessage(msg, 0, b, cfg) // send system notification
+
+		case message == "/quit":
+			msg := fmt.Sprintf("%s has left", getUserName(id))
+			sendMessage(msg, 0, b, cfg)
+			conn.Write([]byte("> Goodbye\n"))
+			conn.Close()
+
+		default:
+			conn.Write([]byte("Unrecognized command.\n"))
+		}
+	}
+
+}
+
+/******************************************
+/* UTILS TODO: move to separate file?
+/******************************************/
+
+func readConfigFromFile() Config {
+
+	byteVal, _ := ioutil.ReadFile("./config.json") // read the entire file into memory
+
+	var cfg Config
+
+	// decode json and store in data map. handle err
+	if err := json.Unmarshal(byteVal, &cfg); err != nil {
+		panic(err)
+	}
+
+	return cfg
+}
+
 func getTimeStamp() (timestamp string) {
 	now := time.Now()
-	// FIXME: handle the error?
-	// t1, _ := time.Parse(
-	// 	time.RFC3339,
-	// 	time.Now())
-	//
-	// return t1.String()
-
-	// test, _ := time.Parse("15:04:05 MST", fmt.Sprintf("05:00:00 %s"))
 
 	timestamp = fmt.Sprintf("%d:%d", now.Hour(),
 		now.Minute())
@@ -291,10 +277,6 @@ func hasUserNameBeenTaken(name string) bool {
 	return false
 }
 
-func getMuteList(id int) map[int]string {
-	return state[id].muteList
-}
-
 // searches through mutelist of a user. Returns true if msg should be muted
 func isMessageAuthorMuted(userIdRequestingMute int, messageAuthorId int) bool {
 
@@ -314,7 +296,6 @@ func addNewUser(userName string) int {
 	newUser := User{name: userName, id: len(state), muteList: map[int]string{}}
 	state = append(state, newUser)
 
-	// FIXME: add error state for dupes
 	userNameList[newUser.name] = newUser.id
 
 	return len(state) - 1
@@ -322,14 +303,12 @@ func addNewUser(userName string) int {
 
 // look up username
 func getUserName(id int) string {
-	// FIXME: check for index out of range?
 	userName := state[id].name
 
 	if userName != "" {
 		return userName
 	}
 
-	// FIXME: generate fun anon names
 	userName = "Anon" + strconv.Itoa(id)
 	return userName
 }
@@ -339,33 +318,25 @@ func getMsgHistory() string {
 	return strings.Join(msgHistory, "")
 }
 
-// add messages to history. limits history to 29 messages
-// FIXME: Consider using channels in here?
+// add messages to history. limits history to 30 messages
 func updateMsgHistory(msg string) {
-
 	// remove first item, if message history is 30
 	if len(msgHistory) > 29 {
 		msgHistory = append(msgHistory[1:], msg)
 	} else {
 		msgHistory = append(msgHistory, msg)
 	}
-
 }
-
-// func getConnectedUsers() []string {
-// 	keys := make([]string, 0, len(state))
-// 	for k := range state {
-// 		keys = append(keys, k)
-// 	}
-//
-// 	return strings.Join(keys)
-// }
 
 func getIdFromUserName(userName string) int {
 	return userNameList[userName]
 }
 
-// adds list of users to mute as slice for user info...
+func getMuteList(id int) map[int]string {
+	return state[id].muteList
+}
+
+// adds list of users to mute
 func muteUser(requestorId int, userToMute string) {
 	idToMute := getIdFromUserName(userToMute)
 	state[requestorId].muteList[idToMute] = "" // hash map for quick and easy access. Don't care about the value
@@ -378,15 +349,13 @@ func unMuteUser(requestorId int, userToUnMute string) {
 
 // adds the metadata to the message
 func formatMessage(msg string, id int) string {
-	// look up the user if their name has been set...
-
 	formattedMsg := fmt.Sprintf("%s [%s] %s\n", getTimeStamp(), getUserName(id), msg)
 	return formattedMsg
 }
 
 // format and send message to all connected clients
-// sends messages to all the connected clients
 func sendMessage(msg string, id int, b *Broker, cfg Config) {
+	// avoid phantom message when user disconnects. TODO: probably need to clean up channel
 	if msg == "" {
 		return
 	}
@@ -412,133 +381,3 @@ func saveLogToFile(msg string, logFilePath string) {
 	defer f.Close()
 	fmt.Fprintf(f, msg)
 }
-
-func handleCommand(message string, conn net.Conn, id int, b *Broker, cfg Config) {
-
-	// if len(queue) > 0 {
-	// 	// msg := <-queue
-	// 	other := "[Jamis] " + <-queue + "\n"
-	// 	conn.Write([]byte(other))
-	// }
-
-	if len(message) > 0 && message[0] == '/' {
-		switch {
-
-		case message == "/help":
-
-			msg := `> Commands you can run:
-/help - See commands available to you
-/history - See the last 30 messages
-/name - Change your username
-/mute [name] - mute another user
-/unmute [name] - unmute a user you have muted previously
-/quit - disconnect your client` + "\n"
-			// msg = formatMessage(msg, 0)
-			conn.Write([]byte(msg))
-
-		case message == "/history":
-			resp := strings.Join(msgHistory, "")
-			// fmt.Print("> History: \n" + resp)
-			resp = "> Recent messages: \n" + resp
-			conn.Write([]byte(resp))
-
-		// starts with /name
-		case strings.HasPrefix(message, "/name "):
-			oldName := getUserName(id)
-			newName := strings.Replace(message, "/name ", "", -1)
-
-			nameTaken := hasUserNameBeenTaken(newName)
-
-			// FIXME: add tests, return error message if taken, or if invalid...
-			// FIXME: verify that name isn't taken yet
-			// success / failure?
-			if nameTaken == true {
-
-				msg := "I'm sorry. The userName [" + newName + "] has already been taken. Try another name"
-				msg = formatMessage(msg, 0) // system msg to 1 person only
-				conn.Write([]byte(msg))
-
-				return
-			}
-
-			changeUserName(id, newName)
-
-			// success msg
-			// send as system message
-			msg := fmt.Sprintf("%s has changed their name to %s", oldName, newName)
-			sendMessage(msg, 0, b, cfg) // send system notification
-
-			// Success / failure FIXME:
-			// conn.Write([]byte(resp))
-
-		case strings.HasPrefix(message, "/mute "):
-			userToMute := strings.Replace(message, "/mute ", "", -1)
-
-			muteUser(id, userToMute)
-
-			// success msg
-			msg := fmt.Sprintf("%s has muted %s", getUserName(id), userToMute)
-			sendMessage(msg, 0, b, cfg) // send system notification
-
-		case strings.HasPrefix(message, "/unmute "):
-			userToUnMute := strings.Replace(message, "/unmute ", "", -1)
-
-			unMuteUser(id, userToUnMute)
-
-			// success msg
-			msg := fmt.Sprintf("%s has UN-muted %s", getUserName(id), userToUnMute)
-			sendMessage(msg, 0, b, cfg) // send system notification
-			// fmt.Println(state)
-
-		// Success / failure FIXME:
-		// conn.Write([]byte(resp))
-
-		case message == "/quit":
-			fmt.Println("Quitting.")
-			msg := fmt.Sprintf("%s has left", getUserName(id))
-			sendMessage(msg, 0, b, cfg)
-			conn.Write([]byte("> Goodbye\n"))
-			conn.Close()
-
-		default:
-			conn.Write([]byte("Unrecognized command.\n"))
-		}
-	}
-
-}
-
-type Config struct {
-	Host     string `json:"host"`
-	Port     string `json:"port"`
-	ConnType string `json:"connType"`
-	LogPath  string `json:"logFilePath"`
-}
-
-func readConfigFromFile() Config {
-
-	byteVal, _ := ioutil.ReadFile("./config.json") // read the entire file into memory
-
-	// map of arbitrary data types (we can prolly optimize this later...)
-	// var data map[string]interface{}
-	var cfg Config
-
-	// decode json and store in data map. handle err
-	if err := json.Unmarshal(byteVal, &cfg); err != nil {
-		panic(err)
-	}
-
-	return cfg
-}
-
-// func handleConnection(conn net.Conn) {
-// 	// Make a buffer to hold incoming data.
-// 	buf := make([]byte, 1024)
-// 	// Read the incoming connection into the buffer.
-// 	reqLen, _ := conn.Read(buf)
-//
-// 	conn.Write(int(reqLen))
-// 	conn.Write([]byte("Message received."))
-// 	conn.Close()
-//
-// 	os.Exit(1)
-// }
